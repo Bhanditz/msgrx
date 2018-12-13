10774ff517c290c9829fbbe215ff80615422368f
Lucien <lygm1062@gmail.com>
2018-12-13 11:14:58 +0800

    cb636f5 master

---
+++ precache-manifest.0b502fdadea9c2054996d0b9a528f4c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d5357a88275c65219ba",
+    "url": "/static/css/main.32c77c93.chunk.css"
+  },
+  {
+    "revision": "1d5357a88275c65219ba",
+    "url": "/static/js/main.1d5357a8.chunk.js"
+  },
+  {
+    "revision": "230c0af60b5cd21a018d",
+    "url": "/static/css/1.b35c408e.chunk.css"
+  },
+  {
+    "revision": "230c0af60b5cd21a018d",
+    "url": "/static/js/1.230c0af6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "53dbc2b7669fd7d05ec484b88ad86987",
+    "url": "/index.html"
+  }
+];
